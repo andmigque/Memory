0c4270d17ab0aed0c8217f3aa0ee2479c5282515
DESKTOP-MHDOKOL\Dre <andmigque@gmail.com>
2026-07-29 18:11:01 -0400

    # Updated Start-Claude with comments describing usage

--- hooks/Start-Claude.ps1
+++ hooks/Start-Claude.ps1
@@ -1,3 +1,5 @@
+#### # Start-Claude.ps1
+#### # 1. Intended to be executed from the users root claude directory, NEVER the project directory.
 . (Join-Path $PSScriptRoot "skills" "get-memory" "scripts" "Get-Memory.ps1")
 $context = Get-Memory | ForEach-Object {
 @"
@@ -8,6 +10,9 @@ $($_.notes)
 "@
 }
 
+#### # 2. Forcces Claude to use Powershell instead of Bash.  
+#### # 3. Expects AGENTS.md to be in root claude dir
+#### # 4. Appends the retrieved context as comma separated notes. 
 claude --tools "CronCreate, CronDelete, CronList, EnterPlanMode, ExitPlanMode, Agent, Monitor, Skill, TaskCreate, TaskGet, TaskList, TaskOutput, TaskOutput, WebFetch, WebSearch, ToolSearch, AskUserQuestion,EnterWorktree,ExitWorkTree,ListMcpResourcesTool,LSP,Monitor,ReadMcpResourcesTool,SendMessage,TodoWrite" `
 --system-prompt-file (Join-Path $HOME '.claude' 'AGENTS.md') `
 --append-system-prompt ($context -join ',')
